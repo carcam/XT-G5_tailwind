9ca0739fb60a69372a4a0a7c279d44ea89409237
anibalsanchez <anibal.sanchez@extly.com>
2020-01-30 15:30:05 +0100

    Tailwind CSS for Joomla Gantry

--- build/build_core.sh
+++ build/build_core.sh
@@ -9,9 +9,9 @@ cd ..
 rm -rf template/node_modules
 
 # Stylesheet to be included inline
-cat template/dist/main.*.css > template/tailwind/compiled/css/xtg5_tailwind.css
+cat template/tailwind/dist/main.*.css > template/tailwind/compiled/css/xtg5_tailwind.css
 
 # JavaScript to be deferred
-cat template/dist/main.js > template/tailwind/compiled/js/xtg5_tailwind.js
+cat template/tailwind/dist/main.js > template/tailwind/compiled/js/xtg5_tailwind.js
 
 npm run build
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -9,38 +9,49 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+const configProxy = require('./config-proxy.json');
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './tailwind/src/styles.css',
+  stats: { warnings: false }, // Hide warnings
   output: {
-		path: path.resolve(__dirname, 'tailwind/dist'),
-	},
+    path: path.resolve(__dirname, 'tailwind/dist')
+  },
   mode: process.env.NODE_ENV,
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            hmr: process.env.NODE_ENV === 'development',
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development'
+            }
           },
-        },
-        'css-loader',
-        'postcss-loader',
-      ],
-    }, ],
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'tailwind/src/index.html',
+      template: 'tailwind/src/index.html'
     }),
-  ],
+    new BrowserSyncPlugin({
+      proxy: {
+        target: configProxy.proxyURL
+      },
+      files: ['tailwind/src/*.*'],
+      cors: true,
+      reloadDelay: 0
+    })
+  ]
 };
